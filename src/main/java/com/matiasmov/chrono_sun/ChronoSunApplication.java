8169fa5e15832e9baa8dcbb03a250d7ccd51d4a5
Lucas Matias <lucasmatias2005@gmail.com>
2026-02-21 01:15:38 -0300

    Initial commit

---
+++ src/main/java/com/matiasmov/chrono_sun/ChronoSunApplication.java
@@ -0,0 +1,13 @@
+package com.matiasmov.chrono_sun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronoSunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronoSunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matiasmov/chrono_sun/ChronoSunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiasmov.chrono_sun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronoSunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ddfbf9d644e33ea004aae0f0b93ec7cccc08a464
Lucas Matias <lucasmatias2005@gmail.com>
2026-02-22 20:58:48 -0300

    ✨ feat: adding the TimeZoneId parameter to the record

--- src/main/java/com/matiasmov/chrono_sun/dto/SunDataResponse.java
+++ src/main/java/com/matiasmov/chrono_sun/dto/SunDataResponse.java
@@ -2,7 +2,8 @@ package com.matiasmov.chrono_sun.dto;
 
 public record SunDataResponse(
     Results results,
-    String status
+    String status,
+    String timeZoneId 
 ) {
     public record Results(
         String sunrise,
